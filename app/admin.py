fa2bf13daef03fd43d27652edcf4154896023756
PabloFernan <pablodgfernandes2005@gmail.com>
2024-11-12 16:10:16 -0300

    herança e inline

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,91 @@
 from django.contrib import admin
-from .models import UF, Generos, Cidades, Editora, Autores, Usuario, Livro, Emprestimo
-
-# Registra os modelos no Django Admin
-admin.site.register(UF)
-admin.site.register(Generos)
-admin.site.register(Cidades)
-admin.site.register(Editora)
-admin.site.register(Autores)
-admin.site.register(Usuario)
-admin.site.register(Livro)
-admin.site.register(Emprestimo)
+from .models import *
 
 
+# Definir as classes de inline
+class CidadeInline(admin.TabularInline):
+    model = Cidade # Modelo relacionado
+    extra = 1 # Quantidade de formulários em branco
+
+
+class LivroInline(admin.TabularInline): # Formulário em tabela
+    model = Livro
+    extra = 1
+
+
+class EmprestimoInline(admin.StackedInline): # Formulário em pilha
+    model = Emprestimo
+    extra = 1
+
+
+class UFAdmin(admin.ModelAdmin):
+    list_display = ("sigla",)
+    search_fields = ("sigla",)
+    ordering = ("sigla",)
+    inlines = [CidadeInline]
+
+
+# Definir as classes de administração
+class CidadeAdmin(admin.ModelAdmin):
+    list_display = ("nome", "uf")
+    list_filter = ("uf",)
+    search_fields = ("nome",)
+    ordering = ("nome",)
+
+
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    list_filter = ("nome",)
+    search_fields = ("nome",)
+    ordering = ("nome",)
+    inlines = [LivroInline]
+
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    list_filter = ("nome",)
+    search_fields = ("nome",)
+    ordering = ("nome",)
+    inlines = [LivroInline]
+
+
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    list_filter = ("nome",)
+    search_fields = ("nome",)
+    ordering = ("nome",)
+    inlines = [LivroInline]
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("nome", "email", "telefone")
+    list_filter = ("nome", "email")
+    search_fields = ("nome", "email")
+    ordering = ("nome", "email")
+
+
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ("autor", "editora", "genero") # Campos a serem exibidos
+    list_filter = ("autor", "editora", "genero") # Campos para filtragem
+    search_fields = ("autor", "editora", "genero") # Campos para busca
+    ordering = ("autor", "editora", "genero") # Campos para ordenação
+    inlines = [EmprestimoInline] # Incluir empréstimos no formulário
+
+
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "livro", "data_sai", "data_ent")
+    list_filter = ("usuario", "livro")
+    search_fields = ("usuario", "livro")
+    ordering = ("usuario", "livro")
+    # inlines = [LivroInline]
+
+
+# Registrar as classes no ambiente administrativo
+admin.site.register(UF, UFAdmin)
+admin.site.register(Cidade, CidadeAdmin)
+admin.site.register(Genero, GeneroAdmin)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Editora, EditoraAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Livro, LivroAdmin)
+admin.site.register(Emprestimo, EmprestimoAdmin)
--- app/models.py
+++ app/models.py
@@ -1,104 +1,252 @@
+
+from typing import Any
 from django.db import models
 
-# Requisito RF08 - Manter UF (MG, SP, RJ, BA, RS)
+
+# Classe 'UF' para representar as Unidades Federativas
 class UF(models.Model):
-    sigla = models.CharField(max_length=2, choices=[
-        ('MG', 'Minas Gerais'),
-        ('SP', 'São Paulo'),
-        ('RJ', 'Rio de Janeiro'),
-        ('BA', 'Bahia'),
-        ('RS', 'Rio Grande do Sul'),
-    ])
+    sigla = models.CharField(
+        max_length=2, # Tamanho máximo da sigla
+        verbose_name="Sigla", # Nome da coluna no ambiente administrativo
+        unique=True, # A sigla deve ser única
+        help_text="Informe a sigla da Unidade Federativa", # Texto de ajuda
+        default="--" # Valor padrão
+    )
 
     class Meta:
-        verbose_name_plural = 'Unidades Federais'
-        
-    def __str__(self):
-        return self.sigla
-
-# Requisito RF02 - Manter Gêneros
-class Generos(models.Model):  
-    nome = models.CharField(max_length=50)  
-
-    class Meta:
-        verbose_name_plural = 'Gêneros dos livros' 
+        verbose_name = "Unidade Federal"  # Nome da classe no singular
+        verbose_name_plural = "Unidades Federais"  # Nome da classe no plural
 
     def __str__(self):
-        return self.nome
+        return self.sigla # Retorna a sigla da UF como string para representar o objeto
+    
 
-# Requisito RF07 - Manter Cidades
-class Cidades(models.Model):  
-    nome = models.CharField(max_length=50)  
-    uf = models.ForeignKey(UF, on_delete=models.CASCADE)  # Nome da chave estrangeira para UF alterado para "uf" (em minúsculo)
+# Classe 'Cidade' para representar as cidades
+class Cidade(models.Model):
+    nome = models.CharField(
+        max_length=255,
+        verbose_name="Nome da Cidade",
+        unique=True,
+        help_text="Informe o nome da cidade",
+        default="--"
+    )
+    uf = models.ForeignKey(
+        UF, # Chave estrangeira para a classe UF
+        on_delete=models.CASCADE, # Ação de exclusão em cascata
+        verbose_name="Unidade Federal",
+        help_text="Selecione a Unidade Federal",
+        default=1, # Valor padrão - UF com sigla 'XX'
+    )
 
     class Meta:
-        verbose_name_plural = 'Cidades'
-
-    def __str__(self):
-        return self.nome
-
-# Requisito RF03 - Manter Editoras
-class Editora(models.Model):
-    nome = models.CharField(max_length=50)  
-    site = models.URLField(max_length=200)  # Melhor usar URLField para o site
-    cidade = models.ForeignKey(Cidades, on_delete=models.CASCADE)  # Nome da chave estrangeira alterado para "cidade"
-
-    class Meta:
-        verbose_name_plural = 'Editoras'
+        verbose_name = "Cidade"
+        verbose_name_plural = "Cidades"
 
     def __str__(self):
         return self.nome
 
-# Requisito RF04 - Manter Autores
-class Autores(models.Model):
-    nome = models.CharField(max_length=50)   
-    cidade = models.ForeignKey(Cidades, on_delete=models.CASCADE)  # Nome da chave estrangeira alterado para "cidade"
+
+# Classe 'Genero' para representar os gêneros literários
+class Genero(models.Model):
+    nome = models.CharField(
+        max_length=255, # Tamanho máximo do nome
+        verbose_name="Nome do Gênero", # Nome da coluna no ambiente administrativo
+        unique=True, # O nome do gênero deve ser único
+        help_text="Informe o nome do gênero literário", # Texto de ajuda
+        default="--" # Valor padrão
+    )
 
     class Meta:
-        verbose_name_plural = 'Autores'
+        verbose_name = "Gênero"
+        verbose_name_plural = "Gêneros"
 
     def __str__(self):
         return self.nome
 
-# Requisito RF01 - Manter Usuários
-class Usuario(models.Model):
-    nome = models.CharField(max_length=50)   
-    cpf = models.CharField(max_length=11)  # CPF é uma string com 11 caracteres
-    data_nasc = models.DateField()  # Definido como DateField
-    email = models.EmailField()
-    telefone = models.CharField(max_length=15)  # Telefone como string para permitir DDD
-    cidade = models.ForeignKey(Cidades, on_delete=models.CASCADE)  # Nome da chave estrangeira alterado para "cidade"
+
+# Hiperclasse 'Pessoa' que será uma classe pai
+class Pessoa(models.Model):
+    nome = models.CharField(
+        max_length=255, 
+        default="--",
+        verbose_name="Nome",
+        help_text="Informe o nome"
+    )
+    email = models.CharField(
+        max_length=255, 
+        default="--",
+        verbose_name="E-mail",
+        help_text="Informe o e-mail",
+    )
+    telefone = models.CharField(
+        max_length=20,
+        default="--",
+        verbose_name="Telefone",
+        help_text="Informe o telefone",
+    )
+    cidade = models.ForeignKey(
+        Cidade, 
+        on_delete=models.CASCADE, 
+        default=1,
+        verbose_name="Cidade",
+        help_text="Selecione a cidade"
+    )
 
     class Meta:
-        verbose_name_plural = 'Usuários'
+        abstract = True
 
     def __str__(self):
         return self.nome
 
-# Requisito RF05 - Manter Livros
+
+# Superclasse que herda de 'Pessoa' e que representa uma pessoa física
+class PessoaFisica(Pessoa):
+    cpf = models.CharField(
+        max_length=11, 
+        default="--",
+        verbose_name="CPF",
+        help_text="Informe o CPF"
+    )
+    data_nasc = models.DateField(
+        default="2000-01-01", # Data padrão no formato AAAA-MM-DD
+        verbose_name="Data de Nascimento",
+        help_text="Informe a data de nascimento"
+    )
+
+    class Meta:
+        abstract = True
+
+
+# Superclasse que herda de 'Pessoa' e que representa uma pessoa jurídica
+class PessoaJuridica(Pessoa):
+    cnpj = models.CharField(
+        max_length=14, 
+        default="--",
+        verbose_name="CNPJ",
+        help_text="Informe o CNPJ"
+    )
+    razao_social = models.CharField(
+        max_length=255, 
+        default="--",
+        verbose_name="Razão Social",
+        help_text="Informe a razão social"
+    )
+    data_fund = models.DateField(
+        default="2000-01-01",
+        verbose_name="Data de Fundação",
+        help_text="Informe a data de fundação"
+    )
+
+    class Meta:
+        abstract = True
+
+
+# Subclasse 'Autor' que herda de 'PessoaFisica'
+class Autor(PessoaFisica):
+    pass
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+
+
+# Subclasse 'Editora' que herda de 'PessoaJuridica'
+class Editora(PessoaJuridica):
+
+    class Meta:
+        verbose_name = "Editora"
+        verbose_name_plural = "Editoras"
+
+
+# Subclasse 'Usuario' que herda de 'PessoaFisica'
+class Usuario(PessoaFisica):
+    pass
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+
+
+# Classe 'Livro' que representa um livro
 class Livro(models.Model):
-    nome = models.CharField(max_length=100)
-    genero = models.ForeignKey(Generos, on_delete=models.CASCADE)
-    autor = models.ForeignKey(Autores, on_delete=models.CASCADE)
-    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
-    preco = models.DecimalField(max_digits=8, decimal_places=2)
-    data_publicacao = models.DateField()
+    nome = models.CharField(
+        max_length=255,
+        default="--",
+        verbose_name="Nome do Livro",
+        help_text="Informe o nome do livro"
+    )
+    genero = models.ForeignKey(
+        Genero, 
+        on_delete=models.CASCADE,
+        verbose_name="Gênero Literário",
+        help_text="Selecione o gênero literário",
+        default=1
+    )
+    autor = models.ForeignKey(
+        Autor, 
+        on_delete=models.CASCADE,
+        verbose_name="Autor",
+        help_text="Selecione o autor",
+        default=1,
+    )
+    editora = models.ForeignKey(
+        Editora, 
+        on_delete=models.CASCADE,
+        verbose_name="Editora",
+        help_text="Selecione a editora",
+        default=1,
+    )
+    preco = models.IntegerField(
+        default=0,
+        verbose_name="Preço",
+        help_text="Informe o preço do livro"
+    )
+    data_pub = models.DateField(
+        default="2000-01-01",
+        verbose_name="Data de Publicação",
+        help_text="Informe a data de publicação"
+    )
 
     class Meta:
-        verbose_name_plural = 'Livros'
+        verbose_name = "Livro"
+        verbose_name_plural = "Livros"
 
     def __str__(self):
         return self.nome
 
-# Requisito RF06 - Manter Empréstimo de Livros
+
+# Classe 'Emprestimo' que representa um empréstimo de um livro
 class Emprestimo(models.Model):
-    data_emprestimo = models.DateField()
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
-    data_devolucao = models.DateField()
+    data_ent = models.DateField(
+        default="2000-01-01",
+        verbose_name="Data de Entrega",
+        help_text="Informe a data de entrega"
+    )
+    data_sai = models.DateField(
+        default="2000-01-01",
+        verbose_name="Data de Saída",
+        help_text="Informe a data de saída"
+    )
+    usuario = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE,
+        verbose_name="Usuário",
+        help_text="Selecione o usuário",
+        default=1   
+    )
+    livro = models.ForeignKey(
+        Livro, 
+        on_delete=models.CASCADE,
+        verbose_name="Livro",
+        help_text="Selecione o livro",
+        default=1
+    )
 
     class Meta:
-        verbose_name_plural = 'Empréstimos'
+        verbose_name = "Empréstimo"
+        verbose_name_plural = "Empréstimos"
 
     def __str__(self):
-        return f'Empréstimo de {self.livro.nome} para {self.usuario.nome}'
+        return f"{self.usuario} pegou {self.livro} emprestado em {self.data_sai} e deve devolver em {self.data_ent}"
+
+    
